56f515aa3d3be567b932b2789349ec01b060af8a
Kangheemyung <kang.2025kensyu@gmail.com>
2025-08-18 14:34:36 +0900

    updateComplete

--- shop/src/main/java/jp/co/sss/shop/controller/ItemController.java
+++ shop/src/main/java/jp/co/sss/shop/controller/ItemController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,25 +13,32 @@ import jp.co.sss.shop.bean.ItemBean;
 import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.form.ItemForm;
 import jp.co.sss.shop.repository.ItemRepository;
-
 @Controller
+@RequestMapping("/items")
 public class ItemController {
 	@Autowired
 	ItemRepository repository;
+//	@RequestMapping("/findItemAndCategory")
+//    public String list(Model model) {
+//        List<ItemWithCategory> list = service.findAll(); // 取得済み想定
+//        model.addAttribute("items", list);
+//        return "items/item_category_list"; // ここがテンプレ名
+//    }
 
-	@RequestMapping("/items/findAll")
+	@RequestMapping("/findAll")
 	public String showItemList(Model model) {
 		model.addAttribute("items", repository.findAll());
 		return "items/item_list";
 	}
 
-	@RequestMapping("/items/findAllByOrderByPriceDesc")
+	@RequestMapping("/findAllByOrderByPriceDesc")
 	public String showItemListByOrderByPriceDesc(Model model) {
 		model.addAttribute("items", repository.findAllByOrderByPriceDesc());
 		return "items/item_list";
 	}
+	
 
-	@RequestMapping("/items/getReferenceById/{id}")
+	@RequestMapping("/getReferenceById/{id}")
 	public String showItem(@PathVariable int id, Model model) {
 		Item item = repository.getReferenceById(id);
 		ItemBean itemBean = new ItemBean();
@@ -41,35 +49,13 @@ public class ItemController {
 		return "items/item";
 	}
 
-	@RequestMapping("/items/findByPrice/{price}")
-	public String showItemListByPrice(@PathVariable Integer price, Model model) {
-		model.addAttribute("items", repository.findByPrice(price));
-		return "items/item_list";
-	}
-
-	@RequestMapping("/items/findByNameAndPrice/{name}/{price}")
-	public String showItemListByNameAndPrice(
-			@PathVariable String name, @PathVariable Integer price, Model model) {
-		model.addAttribute("items", repository.findByNameAndPrice(name, price));
-		return "items/item_list";
-	}
-
-	@RequestMapping("/items/findByNameLike/{name}")
-	public String showItemListByNameLike(@PathVariable String name, Model model) {
-		model.addAttribute("items", repository.findByNameContaining(name));
-		return "items/item_list";
-	}
-	@RequestMapping("/items/findAllAndSetDropdown") 
-	public String itemListSetDropdown(Model model) { 
-	model.addAttribute("items", repository.findAll()); 
-	return "items/item_list_dropdown"; 
-	}
-	 @RequestMapping("/items/create/input") 
+	
+	@RequestMapping("/create/input") 
      public String createInput() { 
          return "items/create_input"; 
      } 
  
-     @RequestMapping(path = "/items/create/complete", method = RequestMethod.POST) 
+	@RequestMapping(path = "/create/complete", method = RequestMethod.POST) 
      public String createComplete(ItemForm form,Model model) { 
         Item item = new Item(); 
         BeanUtils.copyProperties(form, item, "id"); 
@@ -79,7 +65,7 @@ public class ItemController {
         model.addAttribute("item", itemBean); 
         return "items/item"; 
      }
-     @RequestMapping("/items/update/input/{id}") 
+	@RequestMapping("/update/input/{id}") 
      public String updateInput(@PathVariable Integer id, Model model) { 
          Item item =repository.getReferenceById(id); 
          ItemBean itemBean = new ItemBean(); 
@@ -88,7 +74,7 @@ public class ItemController {
          return "items/update_input"; 
      } 
  
-     @RequestMapping(path = "/items/update/complete/{id}", method = RequestMethod.POST) 
+	@RequestMapping(path = "/update/complete/{id}", method = RequestMethod.POST) 
      public String updateComplete(@PathVariable Integer id, ItemForm form, Model model) { 
         Item item = repository.getReferenceById(id); 
         BeanUtils.copyProperties(form, item, "id"); 
@@ -98,12 +84,35 @@ public class ItemController {
         model.addAttribute("item", itemBean); 
         return "items/item"; 
      } 
-     @RequestMapping("/items/delete/input") 
+     @RequestMapping("/findByPrice/{price}")
+ 	public String showItemListByPrice(@PathVariable Integer price, Model model) {
+ 		model.addAttribute("items", repository.findByPrice(price));
+ 		return "items/item_list";
+ 	}
+
+     @GetMapping("/findByNameAndPrice/{name}/{price}")
+ 	public String showItemListByNameAndPrice(
+ 			@PathVariable String name, @PathVariable Integer price, Model model) {
+ 		model.addAttribute("items", repository.findByNameAndPrice(name, price));
+ 		return "items/item_list";
+ 	}
+
+     @GetMapping("/findByNameLike/{name}")
+ 	public String showItemListByNameLike(@PathVariable String name, Model model) {
+ 		model.addAttribute("items", repository.findByNameContaining(name));
+ 		return "items/item_list";
+ 	}
+ 	@RequestMapping("/findAllAndSetDropdown") 
+ 	public String itemListSetDropdown(Model model) { 
+ 	model.addAttribute("items", repository.findAll()); 
+ 	return "items/item_list_dropdown"; 
+ 	}
+     @RequestMapping("/delete/input") 
      public String deleteInput(Model model) { 
          model.addAttribute("items", repository.findAll()); 
          return "items/delete_input"; 
      } 
-     @RequestMapping(path = "/items/delete/complete", method = RequestMethod.POST) 
+     @RequestMapping(path = "/delete/complete", method = RequestMethod.POST) 
      public String deleteComplete(ItemForm form) { 
          repository.deleteById(form.getId()); 
          return "redirect:/items/findAll";
--- shop/src/main/java/jp/co/sss/shop/controller/SessionController.java
+++ shop/src/main/java/jp/co/sss/shop/controller/SessionController.java
@@ -1,25 +1,31 @@
 package jp.co.sss.shop.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.shop.form.LoginFormWithValidation;
 
 @Controller
 public class SessionController {
 	//
-//	@RequestMapping(path = "/login", method = RequestMethod.GET)
-//	public String login() {
-//		return "session/login";
-//	}
+	//	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	//	public String login() {
+	//		return "session/login";
+	//	}
 
 	//ログインのメゾット
-	@RequestMapping(path = "/doLoginOnRequest",method = RequestMethod.POST) 
-	public String doLoginOnRequest(LoginForm form,Model model) { 
-	model.addAttribute("userId",form.getUserId()); 
-	return "session/login_on_request"; 
+	@RequestMapping(path = "/doLoginOnRequest", method = RequestMethod.POST)
+	public String doLoginOnRequest(LoginForm form, Model model) {
+		model.addAttribute("userId", form.getUserId());
+		return "session/login_on_request";
 	}
+
 	@RequestMapping(path = "/loginOnSession", method = RequestMethod.GET)
 	public String loginOnSession() {
 		return "session/login_on_session";
@@ -30,13 +36,32 @@ public class SessionController {
 		if (form.getUserId() == 123) {
 			//入力したユーザID をセッション属性 userId としてセッションスコープに保存 
 			session.setAttribute("userId", form.getUserId());
-			
+
 			return "redirect:/";
 		} else {
 			return "session/login_on_session";
 		}
 	}
 
+	@RequestMapping(path = "/loginWithValidation", method = RequestMethod.GET)
+	public String loginWithValidation(@ModelAttribute LoginFormWithValidation form) {
+		return "session/login_with_validation";
+	}
+	@RequestMapping(path = "/loginWithValidation", method = RequestMethod.POST)
+	public String doLoginWithValidation(
+			@Valid @ModelAttribute LoginFormWithValidation form,
+			BindingResult result, HttpSession session) {
+		if (result.hasErrors()) {
+			return "session/login_with_validation";
+		}
+		if (form.getUserId() == 123) {
+			//入力したユーザID をセッション属性 userId としてセッションスコープに保存 
+			session.setAttribute("userId", form.getUserId());
+			return "redirect:/";
+		} else {
+			return "session/login_with_validation";
+		}
+	}
 	@RequestMapping(path = "/logout", method = RequestMethod.GET)
 	public String logout(HttpSession session) {
 		//セッションの破棄  
@@ -44,5 +69,4 @@ public class SessionController {
 		return "redirect:/";
 	}
 
-	
 }
